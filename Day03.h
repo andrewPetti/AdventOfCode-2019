9837ade58195f951c6bc6a6d71baceae2fa5a222
Andrew <Petti>
2019-12-04 16:45:40 -0700

    Day 3 part 1 is fine
    part 2 is still not wokring

--- Day03.h
+++ Day03.h
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include "inputs03.h"
+#include <map>
 
 namespace Day03
 {
@@ -43,9 +44,11 @@ namespace Day03
 
         std::vector<std::pair<std::string, int>> instructions = GetInstructions(wire);
         auto pt = std::make_pair(0,0);
-
+        auto counter = 0;
+        wirePts.reserve(160000);
         for (auto i : instructions)
         {
+            //counter ++;
             auto action = i.first;
             auto step = i.second;
 
@@ -53,26 +56,37 @@ namespace Day03
             {
                 for (int i = 1; i <= step; i++)
                 {
-                    wirePts.push_back(std::make_pair(pt.first,pt.second+i));
+                    auto curPt = std::make_pair(pt.first,pt.second+i);
+                    wirePts.push_back(curPt);
+
                 }
                 pt.second += step;
             }
             else if (action == "D")
             {
                 for (int i = 1; i <= step; i++)
-                    wirePts.push_back(std::make_pair(pt.first, pt.second-i));
+                {
+                    auto curPt = std::make_pair(pt.first, pt.second-i);
+                    wirePts.push_back(curPt);
+                }
                 pt.second -= step;
             }
             else if (action == "R")
             {
                 for (int i = 1; i <= step; i++)
-                    wirePts.push_back(std::make_pair(pt.first+i, pt.second));
+                {
+                    auto curPt = std::make_pair(pt.first+i, pt.second);
+                    wirePts.push_back(curPt);
+                }
                 pt.first += step;
             }
             else if (action == "L")
             {
                 for (int i = 1; i <= step; i++)
-                    wirePts.push_back(std::make_pair(pt.first-i,pt.second));
+                {
+                    auto curPt = std::make_pair(pt.first-i,pt.second);
+                    wirePts.push_back(curPt);
+                }
                 pt.first -= step;
             }
             else
@@ -80,17 +94,40 @@ namespace Day03
         }
     }
 
-    std::vector<std::pair<int,int>> FindIntercepts(const std::vector<std::pair<int,int>>& wire1, const std::vector<std::pair<int,int>>& wire2)
+    int FindMinDistToPt(std::vector<std::pair<int,int>>& wire, std::pair<int,int>& pt)
     {
-        std::vector<std::pair<int,int>> intercepts;
+        auto counter = 0;
+        std::map<std::pair<int,int>, int> dict;
 
-        for ( auto y=0; y<wire1.size();y++)
+        for (auto wirePt : wire)
         {
-            auto pt1 = wire1[y];
+            counter ++;
+            if ( pt == wirePt)
+                return counter;
+            if (dict.find(wirePt) != dict.end())
+                counter = dict.at(wirePt);
+            else
+                dict[wirePt] = counter;
+        }
+
+        return -1;
+    }
+    std::vector<std::pair<std::pair<int,int>,int>> FindIntercepts(std::vector<std::pair<int,int>>& wire1, std::vector<std::pair<int,int>>& wire2)
+    {
+        std::vector<std::pair<std::pair<int,int>,int>> intercepts;
+
+        for ( int y=0; y<wire1.size();y++)
+        {
+            auto ptY = wire1[y];
             for ( int x= 0; x< wire2.size(); x++)
             {
-                if ( pt1 == wire2[x])
-                    intercepts.push_back(pt1);                
+                auto ptX = wire2[x];
+                if ( ptY == ptX)
+                {
+                    auto distY = FindMinDistToPt(wire1,ptY);
+                    auto distX = FindMinDistToPt(wire2, ptX);
+                    intercepts.push_back(std::make_pair(ptY, distY+distX));                
+                }
             }
         }
         return intercepts;
@@ -100,7 +137,7 @@ namespace Day03
     {
         std::cout << GetText() << std::endl;
         auto inputs = inputs::GetInputs03();
-        //inputs = std::vector<std::string>{"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"};
+        inputs = std::vector<std::string>{"R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"};
 
          auto origin = std::make_pair(0,0 );
  
@@ -117,19 +154,22 @@ namespace Day03
         std::cout << "Intercepts are:" << std::endl;
 
         for (auto& i: intercepts)
-            std::cout << i.first << "," << i.second << std::endl;
+            std::cout << i.first.first << "," << i.first.second <<" - steps "<< i.second<< std::endl;
 
         auto minDist = -1;
-        //std::vector<std::pair<int,int>> wirePts;
-        //std::pair<int,int> origin = std::make_pair(0,0);
+        auto minSteps = -1;
         for (auto& i: intercepts)
         {
-            auto dist = CalcManhattenDistance(origin, i);
+            auto dist = CalcManhattenDistance(origin, i.first);
             if ( minDist == -1 || dist < minDist)
                 minDist = dist;
+            
+            if ( minSteps == -1 || i.second < minSteps)
+                minSteps = i.second;
         }
 
         std::cout << "The closes intercept has a distance of: " << minDist << std::endl;
+        std::cout << "The minimum combined steps to reach an intercept was: " << minSteps << std::endl;
     }
 
 } // namespace Day03
