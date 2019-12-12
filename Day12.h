e8d32857aada6dc123c5e2d74b8aa435acd276b1
Andrew <Petti>
2019-12-12 14:30:17 -0700

    failed attempt Day 12 pt 2
    This crashes after 39.5 million iterations
    but I found a better way

--- Day12.h
+++ Day12.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <iostream>
 #include "inputs12.h"
+#include <map>
 
 namespace Day12
 {
@@ -105,6 +106,69 @@ int CalcEnergyForSteps(const std::vector<std::vector<int>>& startLocation, int s
 
     return totalEnergy;
 }
+
+std::string GetKey(std::vector<int>& info)
+{
+    auto key = std::to_string(info[0])+std::to_string(info[1])+std::to_string(info[2]);
+    return key;
+}
+
+int CalcStepsToRepeat(const std::vector<std::vector<int>>& startLocation)
+{
+    auto vel = std::vector<std::vector<int>>{
+        std::vector<int>{0,0,0},
+        std::vector<int>{0,0,0},
+        std::vector<int>{0,0,0},
+        std::vector<int>{0,0,0}
+    };
+
+    auto pos = startLocation;
+
+    auto cont = true;
+    long long counter = 0;
+    std::map<std::string,std::vector<std::string>> history;
+    for(int j=0;j<4;j++)
+    {
+        auto posKey = std::to_string(j) + GetKey(pos[j]);
+        
+        history.insert(std::make_pair(posKey, std::vector<std::string>{GetKey(vel[j])}));
+    }
+    auto found = std::vector<bool>(4,false);
+    while(cont)
+    {
+        counter++;
+        CalcVelocity(pos, vel);
+        UpdatePositions(pos,vel);
+        if (counter%500000 == 0)
+            std::cout<<"Currently at step: " << counter<<std::endl;
+
+        for ( int i = 0; i<4;i++)
+        {
+            auto posKey = std::to_string(i) + GetKey(pos[i]);
+            auto velKey = GetKey(vel[i]);
+            if (history.find(posKey)== history.end())
+                history.insert(std::make_pair(posKey, std::vector<std::string> {velKey}));
+            else
+            {
+                auto vel = history[posKey];
+                if ( std::find(vel.begin(), vel.end(), velKey) == vel.end())
+                    vel.push_back(velKey);
+                else
+                    found[i]=true;                
+            }    
+        }
+        if ( found[0]==found[1] && found[1]==found[2] && found[2]==found[3] && found[0]== true)
+        {
+            cont = false;
+        }
+        else
+        {
+            found[0]=found[1]=found[2]=found[3]=false;
+        }
+    }
+
+    return  counter;
+}
 void Process()
 {
     std::cout << GetText() << std::endl;
@@ -116,11 +180,18 @@ void Process()
     //     std::vector<int>{4,-8,8},
     //     std::vector<int>{3,5,-1}
     // };
+    input = std::vector<std::vector<int>>{
+        std::vector<int>{-8,-10,0},
+        std::vector<int>{5,5,10},
+        std::vector<int>{2,-7,3},
+        std::vector<int>{9,-8,-3}
+    };
+    //part 1
+    //auto steps = 1000;
+    // auto totalEnergy = CalcEnergyForSteps(input, steps);
+// std::cout<<"The total Energy is: " << totalEnergy <<std::endl;
 
-    auto steps = 1000;
-    auto totalEnergy = CalcEnergyForSteps(input, steps);
-
-    std::cout<<"The total Energy is: " << totalEnergy <<std::endl;
-}
-
+    auto steps = CalcStepsToRepeat(input);
+    std::cout << "The total number of steps before repeating a pos/vel combo for all 4 moons is: " <<steps<<std::endl;    
+    }
 } // namespace Day04
