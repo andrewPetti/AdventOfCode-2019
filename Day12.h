3da23938d321b7106c03973876c5161b4a47b0e2
Andrew <Petti>
2019-12-11 23:19:29 -0700

    Day 12 part 1
    Part 2 might need some big optimiztions.

--- Day12.h
+++ Day12.h
@@ -10,11 +10,117 @@ std::string GetText()
 {
     return "You picked Day 12";
 }
+
+void CalcVelocityChange(const std::vector<int>& x, const std::vector<int>& y, std::vector<int>& xVel, std::vector<int>& yVel)
+{
+    for (int i=0; i<3; i++)
+    {
+        if ( x[i]< y[i])
+        {
+            xVel[i]++;
+            yVel[i]--;
+        }
+        else if (x[i]>y[i])
+        {
+            xVel[i]--;
+            yVel[i]++;
+        }
+    }
+    
+}
+void CalcVelocity(const std::vector<std::vector<int>>& pos, std::vector<std::vector<int>>& vel)
+{
+    auto a = pos[0];
+    auto b = pos[1];
+    auto c = pos[2];
+    auto d = pos[3];
+
+    auto& aVel = vel[0];
+    auto& bVel = vel[1];
+    auto& cVel = vel[2];
+    auto& dVel = vel[3];
+    
+    CalcVelocityChange(a,b,aVel, bVel);
+    CalcVelocityChange(a,c, aVel, cVel);
+    CalcVelocityChange(a,d, aVel,dVel);
+
+    CalcVelocityChange(b,c,bVel,cVel);
+    CalcVelocityChange(b,d,bVel,dVel);
+
+    CalcVelocityChange(c,d,cVel, dVel);
+}
+
+void UpdatePositions(std::vector<std::vector<int>>& pos, const std::vector<std::vector<int>>& vel)
+{
+    for (int i=0;i<4; i++)
+    {
+        auto& x = pos[i];
+        auto& v = vel[i];
+
+        x[0] += v[0];
+        x[1] += v[1];
+        x[2] += v[2];
+    }
+}
+
+int CalcTotalEnergy(const std::vector<std::vector<int>>& pos, const std::vector<std::vector<int>>& vel)
+{
+    auto energy = 0;
+    for (int i=0;i<4;i++)
+    {
+        auto p = pos[i];
+        auto pot = abs(p[0]) +abs(p[1])+abs(p[2]);
+        auto k = vel[i];
+        auto kin = abs(k[0]) +abs(k[1])+abs(k[2]);
+        energy += pot*kin;
+    }
+
+    return energy;
+}
+int CalcEnergyForSteps(const std::vector<std::vector<int>>& startLocation, int steps)
+{
+    auto vel = std::vector<std::vector<int>>{
+        std::vector<int>{0,0,0},
+        std::vector<int>{0,0,0},
+        std::vector<int>{0,0,0},
+        std::vector<int>{0,0,0}
+    };
+
+    auto pos = startLocation;
+
+    for(int i = 1; i< steps+1; i++)
+    {
+        CalcVelocity(pos, vel);
+        UpdatePositions(pos,vel);
+        std::cout<< "Step i: " << i<<std::endl;
+        for(int j=0;j<4;j++)
+        {
+            std::cout<<pos[j][0] << ","<< pos[j][1]<<","<<pos[j][2];
+            std::cout<<" with vel = "<< vel[j][0] << ","<< vel[j][1]<<","<<vel[j][2]<<std::endl;
+        }
+        std::cout<<std::endl;
+    }
+
+    auto totalEnergy = CalcTotalEnergy(pos,vel);
+
+    return totalEnergy;
+}
 void Process()
 {
     std::cout << GetText() << std::endl;
     auto input = inputs::GetInputs12();
 
+    // input = std::vector<std::vector<int>> {
+    //     std::vector<int>{-1,0,2},
+    //     std::vector<int>{2,-10,-7},
+    //     std::vector<int>{4,-8,8},
+    //     std::vector<int>{3,5,-1}
+    // };
+
+    auto steps = 1000;
+    auto totalEnergy = CalcEnergyForSteps(input, steps);
+
+    std::cout<<"The total Energy is: " << totalEnergy <<std::endl;
 }
 
 } // namespace Day04
--- inputs12.h
+++ inputs12.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include <vector>
-
+#include <tuple>
 namespace inputs
 {
-std::pair<int, int> GetInputs12()
+std::vector<std::vector<int>> GetInputs12()
 {
-    return std::make_pair(231832, 767346);
+    auto inputs = std::vector<std::vector<int>>
+    {
+        std::vector<int>{17,-9,4},
+        std::vector<int>{2,2,-13},
+        std::vector<int>{-1,5,-1},
+        std::vector<int>{4,7,-7}
+    };
+    return inputs;
 }
 } // namespace inputs
