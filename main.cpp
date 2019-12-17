9670490edf0aa1b65b6bd41fa07575e1ae8fad62
Andrew <Petti>
2019-12-17 11:57:09 -0700

    fixing merge issue

--- main.cpp
+++ main.cpp
@@ -13,11 +13,7 @@
 #include "Day11.h"
 #include "Day12.h"
 #include "Day13.h"
-<<<<<<< Updated upstream
-
 #include "Day14.h"
-=======
->>>>>>> Stashed changes
 
 int main()
 {
@@ -67,12 +63,9 @@ int main()
     case 13:
         Day13::Process();
         break;
-<<<<<<< Updated upstream
     case 14:
         Day14::Process();
         break;
-=======
->>>>>>> Stashed changes
     default:
         std::cout << "you picked an invalid day" << std::endl;
     }
